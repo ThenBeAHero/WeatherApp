238de57b7966c03a9e82e452b7e3dd38ba289267
kanitam <kanitamurseli83@gmail.com>
2025-05-02 03:02:43 +0200

    Files kopje
    Krijimi i disa fajllave kopje ne menyre qe te perdoren vetem per testime, kompatibil me browser-in

---
+++ assests/tests/api.cjs.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const api_key = "f160cc3e68db509860c45ece941f8e51";
+
+ const fetchData = function (URL, callback) {
+    return global.fetch(`${URL}&appid=${api_key}`)
+      .then(res => res.json())
+      .then(data => callback(data));
+  }
+ const url = {
+    currentWeather(lat, lon) {
+      return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+      return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+      return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+      return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query Search query e.g.: "London", "New York"
+     */
+    geo(query) {
+      return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+  }  
+  module.exports = { fetchData, url };
